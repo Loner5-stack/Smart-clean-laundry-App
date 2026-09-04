68a78131dbe6e7a84daa7abfbbe39d6ea03522b0
Loner5-stack <biggeststeve2006@gmail.com>
2026-09-04 17:39:33 +0100

    set 0

--- smart-clean/src/app/layout.tsx
+++ smart-clean/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Smart-Clean | Logistics Simplified",
@@ -29,6 +30,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <SessionProvider>{children}</SessionProvider>
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
